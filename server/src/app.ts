cc782f3922d2b5cf9528aa1b3c105885fbe88ae9
jaroslavmerta <log.merta@gmail.com>
2022-02-20 22:15:14 +0100

    basic setup , devDependecies @types/mysql

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,18 @@
-console.log("hello world")
+import express, {Request, Response} from 'express';
+import mysql from 'mysql';
+
+const app = express();
+
+app.get('/api/characters', (req: Request, res:Response)=> {
+    res.send("It works");
+})
+
+app.get('/api/characters/:id', (req: Request, res:Response)=> {
+    const id = req.params.id;
+    res.send("It works for id" + id);
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port,  () => {
+    console.log("Server is running")
+})
